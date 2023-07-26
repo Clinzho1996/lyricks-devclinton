11c65c2287e0b9d0571ec33a4f8ca00b7e1bde41
Clinzho1996 <confidinho@yahoo.com>
2023-07-26 18:23:47 +0100

    music app

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -24,7 +24,7 @@ const TopChartCard = ({
   <div
     className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${
       activeSong?.title === song?.title ? "bg-[#4c426e]" : "bg-transparent"
-    } py-2 p-0 rounded-lg cursor-pointer mb-2`}
+    } py-2 p-2 rounded-lg cursor-pointer mb-2`}
   >
     <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
     <div className="flex-1 flex flex-row justify-between items-center">
