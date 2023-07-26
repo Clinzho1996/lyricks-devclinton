ba5904d277cf4240d4a060a730ecd8e6a453d5c2
Clinzho1996 <confidinho@yahoo.com>
2023-07-26 12:32:17 +0100

    music app

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,30 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY
+      );
+
+      return headers;
+    },
+  }),
+
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({
+      query: () => "/charts/track",
+    }),
+    getSongsBySearch: builder.query({
+      query: (searchTerm) =>
+        `/search/?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+    }),
+  }),
+});
+
+export const { useGetTopChartsQuery, useGetSongsBySearchQuery } = shazamCoreApi;
